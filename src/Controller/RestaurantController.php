5d6ba5d591772f9f6d07cff37d23efaa35be9c17
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-03-16 12:02:59 +0100

    Add routes to RestaurantController.php

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -2,18 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RestaurantController extends AbstractController
 {
     /**
-     * @Route("/restaurant", name="restaurant")
+     * Affiche la liste des restaurants
+     * @Route("/restaurants", name="restaurant_index", methods={"GET"})
      */
     public function index()
     {
         return $this->render('restaurant/index.html.twig', [
             'controller_name' => 'RestaurantController',
         ]);
+    }
+
+    /**
+     * Affiche un restaurant
+     * @Route("/restaurant/{restaurant}", name="restaurant_show", methods={"GET"})
+     * @param Restaurant $restaurant
+     */
+    public function show(Restaurant $restaurant)
+    {
+    }
+
+    /**
+     * Affiche le formulaire de création de restaurant
+     * @Route("/restaurant/new", name="restaurant_new", methods={"GET"})
+     */
+    public function new()
+    {
+    }
+
+    /**
+     * Traite la requête d'un formulaire de création de restaurant
+     * @Route("/restaurant", name="restaurant_create", methods={"POST"})
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Affiche le formulaire d'édition d'un restaurant (GET)
+     * Traite le formulaire d'édition d'un restaurant (POST)
+     * @Route("/restaurant/{restaurant}/edit", name="restaurant_edit", methods={"GET", "POST"})
+     * @param Restaurant $restaurant
+     */
+    public function edit(Restaurant $restaurant)
+    {
+    }
+
+    /**
+     * Supprime un restaurant
+     * @Route("/restaurant/{restaurant}", name="restaurant_delete", methods={"DELETE"})
+     * @param Restaurant $restaurant
+     */
+    public function delete(Restaurant $restaurant)
+    {
     }
 }
